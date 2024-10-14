dc7bc8bf3f8f06c0352ee3594c03d7d6eaa55f98
Bartosz <bartoszchmielecki2003@gmail.com>
2024-10-15 00:52:02 +0200

    !

---
+++ Day 33 - react-website/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Day 33 - react-website/my-app/src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
+export default function Navbar() {
+  return (
+    <div className="flex text-white justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className="hidden">
+        <ul>
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
